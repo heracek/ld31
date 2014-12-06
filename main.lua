e83b460578b635e009448bd90a89d8c14ba38e46
Tomáš Horáček <tomas.horacek@gmail.com>
2014-12-06 16:40:30 +0100

    integrated TLfres lib

--- main.lua
+++ main.lua
@@ -1,7 +1,16 @@
+require "lib.TLfres"
+
 function love.load()
+  TLfres.setScreen({w=640, h=480, full=false, vsync=true, aa=0}, 320, false, false)
 end
 
 function love.update(dt)
+end
+
+function love.draw()
+  TLfres.transform()
+
+  TLfres.letterbox(4,3)
 end
 
 function love.keypressed(key)
