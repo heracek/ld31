c1bfffb76990259c271bc201a1f54cf90f055ded
Tomáš Horáček <tomas.horacek@gmail.com>
2014-12-07 15:09:47 +0100

    added basic world boundry

--- Player.lua
+++ Player.lua
@@ -1,18 +1,26 @@
 local Player = class("Player")
 
+Player.static.WORLD_MOVE_BORDER = 16
+Player.static.WORLD_WIDTH       = 320
+
 function Player:initialize()
   self.x, self.y  = 0, 0
+  self.xspeed     = 0
   self.move_speed = 100
 
   self.sprite = Image.characters.character01
 end
 
 function Player:update(dt)
+  self.xspeed = 0
   if Input:isDown("d") then
-    self.x = self.x + dt * self.move_speed
+    self.xspeed = self.move_speed
   elseif Input:isDown("a") then
-    self.x = self.x - dt * self.move_speed
+    self.xspeed = - self.move_speed
   end
+
+  self.x = self.x + self.xspeed * dt
+  self.x = math.max(0, math.min(Player.WORLD_WIDTH - Player.WORLD_MOVE_BORDER, self.x))
 end
 
 function Player:draw()
