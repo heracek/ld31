a388a8341b8fec2003b8ef50e5f886c162d6018e
Tomáš Horáček <tomas.horacek@gmail.com>
2014-12-07 13:04:29 +0100

    added supprotr for hump.gamestate

--- main.lua
+++ main.lua
@@ -1,20 +1,37 @@
 require "lib.TLfres"
+GS = require "lib.hump.gamestate" -- GS is Gamestate
 
 function love.load()
   TLfres.setScreen({w=640, h=480, full=false, vsync=true, aa=0}, 320, false, false)
+  love.graphics.setDefaultFilter('nearest')
+
+  GS.registerEvents()
 end
 
-function love.update(dt)
-end
-
-function love.draw()
+local gamestateDraw = GS.draw
+GS.draw = function()
   TLfres.transform()
-
+  gamestateDraw()
   TLfres.letterbox(4,3)
 end
 
 function love.keypressed(key)
-  if (key == 'escape') then
+  if ('escape' == key or 'q' == key) then
     love.event.quit()
   end
 end
+
+local function LazyValueTable(getValueForKeyCallback)
+  return setmetatable({}, {
+    __index = function(table, key)
+      local value = getValueForKeyCallback(key)
+      table[key] = value
+      return value
+    end
+  })
+end
+
+State = LazyValueTable(function(state) return require('states.'..state) end)
+Image = LazyValueTable(function(directory)
+  return LazyValueTable(function(filename) return love.graphics.newImage('img/'..directory..'/'..filename..'.png') end)
+end)
