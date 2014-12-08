e38df88507aa6b10c59177d9d081d6187487b57e
Tomáš Horáček <tomas.horacek@gmail.com>
2014-12-08 22:23:39 +0100

    added basic gravity

--- Player.lua
+++ Player.lua
@@ -3,12 +3,15 @@ local Player = class("Player", Entry)
 
 Player.static.WORLD_MOVE_BORDER = 16
 Player.static.WORLD_WIDTH       = 320
+Player.static.GRAVITY           = 600
+Player.static.MAX_FALL_SPEED    = 200
 
 function Player:initialize()
   Entry.initialize(self)
   self.x, self.y  = 0, 0
   self.zindex     = 100
   self.xspeed     = 0
+  self.yspeed     = 0
   self.move_speed = 100
 
   self.sprite = Image.characters.character01
@@ -24,6 +27,13 @@ function Player:update(dt)
 
   self.x = self.x + self.xspeed * dt
   self.x = math.max(0, math.min(Player.WORLD_WIDTH - Player.WORLD_MOVE_BORDER, self.x))
+
+  self.yspeed = self.yspeed + Player.GRAVITY * dt
+  self.yspeed = math.min(Player.MAX_FALL_SPEED, self.yspeed)
+  self.y = self.y + self.yspeed * dt
+
+  -- ground collision
+  self.y = math.min(100, self.y)
 end
 
 function Player:draw()
